35abd2726e424f657e49afbf8fcdc30706abfc6b
Soogyung Jung <sally1106@naver.com>
2024-11-21 14:26:38 +0900

    모노레포

---
+++ ch09/monorepo-test/apps/react-app1/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+import { Home } from '@monorepo-test/common';
+
+export function App() {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ ch09/monorepo-test/apps/react-app2/src/app/app.tsx
@@ -0,0 +1,17 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import { Common, Home } from '@monorepo-test/common';
+export function App() {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/common">Common</Link>
+      </div>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/common" element={<Common />}></Route>
+      </Routes>
+    </>
+  );
+}
+export default App;
